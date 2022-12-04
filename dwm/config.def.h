7030aff74ce183f1b6dd3669d7b6dc4f096da1e9
KoKoKotlin <labruzzler@gmail.com>
2022-12-04 14:57:30 +0100

    changed dwm colors

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -8,11 +8,11 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "JetBrains Mono NL:size=10", "Symbola:style=Regular", "Inconsolata:style=Medium", "Emoji One:style=Regular"};
 static const char dmenufont[]       = "JetBrains Mono NL:size=10";
-static const char col_gray1[]       = "#222222";
+static const char col_gray1[]       = "#1d1f21"
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_cyan[]        = "#A21E0B";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
