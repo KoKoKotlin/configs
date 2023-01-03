efc5ecc1c87f11c481ce9d4b7fd932660d1ddc06
Yannik Höll <yannik.hoell@stundent.tu-freiberg.de>
2023-01-03 18:48:44 +0100

    added xrandr config to autostart.sh

--- autostart.sh
+++ autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-picom
-nitrogen --restore
-run slstatus &
-
+xrandr --output DP-0 --right-of HDMI-0
+picom &
+slstatus &
+feh --bg-fill ~/Pictures/958729-Arch-Linux-Linux-open-source.png
